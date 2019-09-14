c9c8eae54b9428d09db356e68a0c4ac2fab929b0
Josue Alejandro Diaz Rojas <Patchinster@Gmail.com>
2019-09-14 11:09:55 -0500

    salu4

---
+++ build/precache-manifest.cb7f2ba5812521dd4977f20fc2dcc622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f6373eeeae735ffbac195ca4de9dda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfd31861aad55300981f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "31e159844cd3971be63c",
+    "url": "/static/js/2.14e5e52e.chunk.js"
+  },
+  {
+    "revision": "dfd31861aad55300981f",
+    "url": "/static/js/main.f647a7af.chunk.js"
+  },
+  {
+    "revision": "73b72eafb89ab0089549",
+    "url": "/static/js/runtime~main.da1a12be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
